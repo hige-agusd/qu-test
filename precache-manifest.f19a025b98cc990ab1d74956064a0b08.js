31d603b1df567247e9569872d9e1e6695819d1f6
hige-agusd <diazagustin@gmail.com>
2020-07-15 14:39:19 -0300

    Use master as gh page

---
+++ precache-manifest.f19a025b98cc990ab1d74956064a0b08.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0be79cc42d775c62d15",
+    "url": "/starwars-atlas/static/css/main.ae3d72c5.chunk.css"
+  },
+  {
+    "revision": "a0be79cc42d775c62d15",
+    "url": "/starwars-atlas/static/js/main.d8a35430.chunk.js"
+  },
+  {
+    "revision": "f1429e8e840c940fed22",
+    "url": "/starwars-atlas/static/js/runtime~main.a2d535c8.js"
+  },
+  {
+    "revision": "c13d30ca578b5d8dba25",
+    "url": "/starwars-atlas/static/js/2.9a1af60a.chunk.js"
+  },
+  {
+    "revision": "edf7ddabf61d7ae2929189a71e69c5f4",
+    "url": "/starwars-atlas/static/media/loading-kylo-saber.edf7ddab.gif"
+  },
+  {
+    "revision": "d1736a532f80cb0ad544d89cafa36884",
+    "url": "/starwars-atlas/static/media/SF Distant Galaxy Alternate.d1736a53.ttf"
+  },
+  {
+    "revision": "f0b6ea67ba06de3f6770e5d93555fef7",
+    "url": "/starwars-atlas/static/media/SF Distant Galaxy AltOutline.f0b6ea67.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-atlas/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "dfccdfd940d3270e369fd46d44ae0511",
+    "url": "/starwars-atlas/index.html"
+  }
+];
